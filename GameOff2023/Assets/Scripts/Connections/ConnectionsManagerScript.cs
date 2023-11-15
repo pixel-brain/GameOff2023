07a05161efb8785fc3c1d2a424673e00b44585a6
NukoTheNeko <36847623+NukoTheNeko@users.noreply.github.com>
2023-11-15 20:25:24 +0000

    Created the base for the connections
    Created input points, output points, a system to create links between these and also refresh them while moving the objects around.
    To-do: make things follow the drawn path on start.

---
+++ GameOff2023/Assets/Scripts/Connections/ConnectionsManagerScript.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct PathInfo
+{
+    private Transform _inputTransform;
+    private Transform _outputTransform;
+
+    public PathInfo(Transform inputTransform, Transform outputTransform)
+    {
+        _inputTransform = inputTransform;
+        _outputTransform = outputTransform;
+    }
+    public Transform OutputTransform { readonly get => _outputTransform; set => _outputTransform = value; }
+    public Transform InputTransform { readonly get => _inputTransform; set => _inputTransform = value; }
+}
+public class ConnectionsManagerScript : MonoBehaviour
+{
+	[Header("Required Components")]
+	[Space]
+
+	[SerializeField]
+	private LayerMask _inputOutputLayer;
+
+	private LineRenderer _lineRenderer;
+
+	
+	[Header("Visualization")]
+	[Space]
+	[SerializeField]
+	private List<PathInfo> _paths;
+
+	private Transform _startPoint = null; 
+
+	private void Awake()
+	{
+		_lineRenderer = transform.GetComponent<LineRenderer>();
+	}
+
+	private void Update()
+	{
+		if(!_lineRenderer)
+        {
+            Debug.LogError("A Line Renderer is required for this object to function: " + gameObject.name);
+            return;
+        }
+		if(_startPoint)
+		{
+			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			_lineRenderer.positionCount = 4;
+			_lineRenderer.SetPosition(0, new Vector3(_startPoint.position.x, _startPoint.position.y, 0));
+			_lineRenderer.SetPosition(1, new Vector3(_startPoint.position.x, (float)Math.Round(mousePos.y - ((mousePos.y - _startPoint.position.y) / 2)), 0));
+			_lineRenderer.SetPosition(2, new Vector3(mousePos.x, (float)Math.Round(mousePos.y - ((mousePos.y - _startPoint.position.y) / 2)), 0));
+			_lineRenderer.SetPosition(3, new Vector3(mousePos.x, mousePos.y, 0));
+		}
+		else
+		{
+			_lineRenderer.positionCount = 0;
+		}
+		if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit2D hitInfo = Physics2D.Raycast(ray.origin, ray.direction,Mathf.Infinity,_inputOutputLayer);
+			if (hitInfo)
+			{
+				Transform objectSelected = hitInfo.collider.transform;
+				_startPoint = objectSelected;
+			}
+		}
+		if (Input.GetKeyUp(KeyCode.Mouse0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit2D hitInfo = Physics2D.Raycast(ray.origin, ray.direction,Mathf.Infinity,_inputOutputLayer);
+			if (hitInfo)
+			{
+				Transform objectSelected = hitInfo.collider.transform;
+				if(_startPoint != null && !objectSelected.CompareTag(_startPoint.tag))
+				{
+					CreatePath(objectSelected);
+				}
+			}
+			_startPoint = null;
+		}
+    }
+
+	private void CreatePath(Transform endPoint)
+	{
+		PathInfo pathInfo = new(null, null);
+		switch(_startPoint.tag)
+		{
+			case "Input":
+				pathInfo.InputTransform = _startPoint;
+				pathInfo.OutputTransform = endPoint;
+				break;
+			case "Output":
+				pathInfo.InputTransform = endPoint;
+				pathInfo.OutputTransform = _startPoint;
+				break;
+			default:
+				Debug.LogWarning("Tried to create a path with something invalid");
+				return;
+		}
+		int index = _paths.FindIndex(path => path.InputTransform == pathInfo.InputTransform);
+		if(index >= 0)
+		{
+			_paths.RemoveAt(index);
+		}
+		PathDrawerScript drawerScript = pathInfo.InputTransform.GetComponent<PathDrawerScript>();
+		drawerScript.AssignPath(pathInfo);
+		_paths.Add(pathInfo);
+	}
+}
